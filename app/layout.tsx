332ad4efbcfc3fcc2645006fc10678841ad7468f
Praise <praiseibec@gmail.com>
2025-11-05 22:26:37 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 const _geist = Geist({ subsets: ["latin"] })
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans antialiased`}>{children}</body>
+      <Analytics />
     </html>
   )
 }
